426b342fd6c1b3122d73afcb4326918928f27d29
Dan Haas <dan.z.haas@gmail.com>
2020-05-30 10:58:16 -0400

    workshop 1

--- node-express/server.js
+++ node-express/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,28 +15,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
-
-// app.get('/campsites/:campsiteId', (req, res)  => {
-//     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-// })
-
-// app.post('/campsites/:campsiteId', (req, res) => {
-//     res.statusCode = 403;
-//     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-// })
-
-// app.put('/campsites/:campsiteId', (req, res) => {
-//     res.write(`Updating the campsite: ${req.params.campsiteId} \n`);
-//     res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`)
-// })
-
-// app.delete('/campsites/:campsiteId', (req, res) => {
-//     res.end(`Deleting campsite: ${req.params.campsiteId}`);
-// })
-
-
-// __dirname is a special variable; returns to absolute path of current directory
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
