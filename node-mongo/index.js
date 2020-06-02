62e25b8e0978bfab37d11f60a66e93708ba92e18
Dan <dan.z.haas@gmail.com>
2020-06-02 19:01:54 -0400

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,10 +4,11 @@ const assert = require('assert').strict;
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
+const dboper = require('./operations');
 
 //connects to mongoDB server and callback w client object to access database
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-    
+
     assert.strictEqual(err, null);
 
     console.log('Connected correctly to server');
@@ -19,20 +20,27 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         assert.strictEqual(err, null);
         console.log('Dropped Collection', result);
 
-        const collection = db.collection('campsites');
-
         //inserted new document into db
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
-        (err, result) => {
-            assert.strictEqual(err, null);
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites', result => {
             console.log('Insert Document:', result.ops);
 
-            //logs all documents in the campsite collection
-            collection.find().toArray((err, docs) => {
-                assert.strictEqual(err, null);
-                console.log('Found Documents:', docs)
+            dboper.findDocuments(db, 'campsites', (docs) => {
+                console.log('Found Documents:', docs);
 
-                client.close();
+                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, { description: "Update Test Description" }, 'campsites', result => {
+                    console.log('Updated Document Count:', result.result.nModified);
+
+                    dboper.findDocuments(db, 'campsites', docs => {
+                        console.log('Found Documents:', docs);
+
+                        dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites', result => {
+                            console.log('Deleted Document Count:', result.deletedCount);
+
+                            client.close();
+
+                        });
+                    });
+                });
             });
         });
     });
