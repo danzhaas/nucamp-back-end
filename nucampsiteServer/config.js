8d34020179ec64a72eed43d8522363028e667f45
Dan <dan.z.haas@gmail.com>
2020-06-17 13:09:42 -0400

    passport facebook

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId:'3467280413296363',
+        clientSecret: '3720ca07d4f166bbc9377db5bd0b4b85'
+    }
 }
